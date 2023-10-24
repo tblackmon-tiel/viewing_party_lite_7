a7b9cf646398385f44f7cc38d286157ebabef78a
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-10-24 16:27:24 -0600

    refactor user controller actions to simplify conditionals

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,14 +14,11 @@ class UsersController < ApplicationController
     params = user_params
     params[:email] = params[:email].downcase
     new_user = User.new(params)
-    if !(params[:password].present? && params[:password_confirmation].present?) || params[:password] != params[:password_confirmation]
-      flash[:error] = 'Your password and confirmation did not match!'
-      render :new
-    elsif new_user.save
+    if new_user.save
       flash[:success] = 'New account created successfully.'
       redirect_to user_path(new_user)
     else
-      flash[:error] = 'Please fill out the entire form!'
+      flash[:error] = new_user.errors.full_messages.to_sentence
       render :new
     end
   end
@@ -31,10 +28,7 @@ class UsersController < ApplicationController
 
   def login
     user = User.find_by(email: params[:email])
-    if !user
-      flash[:error] = "Sorry, your credentials are incorrect!"
-      render :login_form
-    elsif user.authenticate(params[:password])
+    if user&.authenticate(params[:password])
       flash[:success] = "Logged in successfully!"
       redirect_to user_path(user)
     else
